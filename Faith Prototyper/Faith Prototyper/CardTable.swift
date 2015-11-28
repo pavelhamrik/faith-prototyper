36b9679ce52fa038af4cc2b274a7a2c9fae8be22
Pavel Hamřík <pavel.hamrik@gmail.com>
2015-11-28 14:18:23 +0100

    Core Text and typesetting in general started.

--- Faith Prototyper/Faith Prototyper/CardTable.swift
+++ Faith Prototyper/Faith Prototyper/CardTable.swift
@@ -15,17 +15,36 @@ class CardTable: NSViewController, NSTableViewDelegate, NSTableViewDataSource {
     ]
     
     @IBOutlet var tableView: NSTableView!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         NSNotificationCenter.defaultCenter().addObserver(self, selector: "act:", name:"refreshCardTableView", object: nil)
+        
+        let defaultsData = NSUserDefaults.standardUserDefaults().dataForKey("DefaultCards")
+        if (defaultsData != nil) {
+            let storedDictionary = NSKeyedUnarchiver.unarchiveObjectWithData(defaultsData!)
+            self.rows = storedDictionary!.mutableCopy() as! [NSMutableDictionary]
+            fillTable()
+        }
     }
     
+    
     func act(notification: NSNotification) {
+        
         let userinfo = notification.userInfo
         self.rows = userinfo?["rows"] as! [NSMutableDictionary]
         
-        // store for lates
+        let defaults = NSUserDefaults.standardUserDefaults()
+        let rowsAsData = NSKeyedArchiver.archivedDataWithRootObject(self.rows)
+        defaults.setObject(rowsAsData, forKey: "DefaultCards")
+        defaults.synchronize()
+        
+        fillTable()
+    }
+    
+    
+    func fillTable() {
         let tableCols = self.tableView.tableColumns
         for col in tableCols {
             self.tableView.removeTableColumn(col)
@@ -40,16 +59,19 @@ class CardTable: NSViewController, NSTableViewDelegate, NSTableViewDataSource {
         
         self.tableView.reloadData()
     }
+    
 
     func numberOfRowsInTableView(aTableView: NSTableView) -> Int {
         let numberOfRows:Int = getDataArray().count
         return numberOfRows
     }
     
+    
     func tableView(tableView: NSTableView, objectValueForTableColumn tableColumn: NSTableColumn?, row: Int) -> AnyObject? {
         let newString = getDataArray().objectAtIndex(row).objectForKey(tableColumn!.identifier)
         return newString;
     }
+    
     
     func getDataArray() -> NSArray {
         var regularRows = self.rows
--- Faith Prototyper/Faith Prototyper/ViewController.swift
+++ Faith Prototyper/Faith Prototyper/ViewController.swift
@@ -15,9 +15,16 @@ class ViewController: NSViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        print(tmpDirURL)
+        //print(tmpDirURL)
+        
+        // prefill self.rows with previously stored data
+        let defaultsData = NSUserDefaults.standardUserDefaults().dataForKey("DefaultCards")
+        if (defaultsData != nil) {
+            let storedDictionary = NSKeyedUnarchiver.unarchiveObjectWithData(defaultsData!)
+            self.rows = storedDictionary!.mutableCopy() as! [NSMutableDictionary]
+        }
     }
-
+    
     override var representedObject: AnyObject? {
         didSet {
             // Update the view, if already loaded.
@@ -76,7 +83,7 @@ class ViewController: NSViewController {
                             sheets[sheetAttrName!] = workbookRels[rid!]
                         }
                     }
-
+                    
                     // xl/sharedStrings.xml
                     xmlURL = self.tmpDirURL.URLByAppendingPathComponent("xl/sharedStrings.xml")
                     xmlToParse = try! NSString(contentsOfURL: xmlURL, encoding: NSUTF8StringEncoding)
@@ -104,9 +111,11 @@ class ViewController: NSViewController {
                         allColsSet = allColsSet.union(cols)
                     }
                     let keys = Array(allColsSet)
+                    //keys = keys.sort({ $0 < $1 })
                     let keyset = NSDictionary.sharedKeySetForKeys(keys)
                     
                     // the full stack of cards
+                    self.rows.removeAll() // remove all rows to overwrite the data stored in defaults
                     for (_, value) in sheets {
                         xmlURL = self.tmpDirURL.URLByAppendingPathComponent("xl/" + value)
                         xmlToParse = try! NSString(contentsOfURL: xmlURL, encoding: NSUTF8StringEncoding)
@@ -119,6 +128,8 @@ class ViewController: NSViewController {
                                 localKeys.append(sharedStrings[index!])
                             }
                         }
+                        //localKeys = localKeys.sort({ $0 < $1 })
+                        
                         var xmlRowNum = 0
                         for xmlRow in xml["worksheet"]["sheetData"]["row"] {
                             xmlRowNum += 1
@@ -174,35 +185,164 @@ class ViewController: NSViewController {
                 
                 if (exportedFileURL?.isFileReferenceURL() != nil) {
                     
-                    var pageSize = CGRect(x: 0.0, y: 0.0, width: 612, height: 792)
+                    // page size
+                    let pgxsize = CGFloat(595.0)
+                    let pgysize = CGFloat(842.0)
                     
+                    // card size
+                    let cardxsize = CGFloat(self.mm2pt(63))
+                    let cardysize = CGFloat(self.mm2pt(88))
+                    
+                    // page layout
+                    let cardxgutter = CGFloat(self.mm2pt(3))
+                    let cardygutter = CGFloat(self.mm2pt(3))
+                    let minpgxmargin = CGFloat(self.mm2pt(5))
+                    let minpgymargin = CGFloat(self.mm2pt(8))
+                    
+                    var pageSize = CGRect(x: 0.0, y: 0.0, width: pgxsize, height: pgysize)
+                    
+                    let cardsperx = floor((pgxsize - (2 * minpgxmargin) + cardxgutter) / (cardxsize + cardxgutter))
+                    let cardspery = floor((pgysize - (2 * minpgymargin) + cardygutter) / (cardysize + cardygutter))
+                    
+                    let pgxmargin = (pgxsize - ((cardsperx * cardxgutter - cardxgutter) + (cardsperx * cardxsize))) / 2
+                    let pgymargin = (pgysize - ((cardspery * cardygutter - cardygutter) + (cardspery * cardysize))) / 2
+                    
+                    // context
                     let context = CGPDFContextCreateWithURL(exportedFileURL, &pageSize, nil)
-
                     
-                    CGPDFContextBeginPage(context, nil)
+                    // temporary workaround
+                    if (self.rows.count < 1) {
+                        for _ in 1...12 {
+                            self.rows.append(["fname": "A", "lname": "B"])
+                        }
+                    }
+                    // /temporary workaround
                     
-                    // http://www.techotopia.com/index.php/Drawing_iOS_8_2D_Graphics_in_Swift_with_Core_Graphics
-                    // Graphic Contexts Doc https://developer.apple.com/library/mac/documentation/GraphicsImaging/Conceptual/drawingwithquartz2d/dq_context/dq_context.html
-                    // Processing Images Doc https://developer.apple.com/library/ios/documentation/GraphicsImaging/Conceptual/CoreImaging/ci_tasks/ci_tasks.html
-                    // Learn Quartz http://cocoadevcentral.com/d/intro_to_quartz/
+                    let pgnums = Int(ceil(CGFloat(self.rows.count) / (cardsperx * cardspery)))
+                    for var pgnum = 0; pgnum < pgnums; pgnum += 1 {
+                        CGPDFContextBeginPage(context, nil)
+                        for var pgrow = CGFloat(0.0); pgrow < cardsperx; pgrow += 1 {
+                            for var pgcol = CGFloat(0.0); pgcol < cardspery; pgcol += 1 {
+                                
+                                var cardxbound = CGFloat(cardxsize * pgcol + pgxmargin)
+                                if (pgcol >= CGFloat(1)) {
+                                    cardxbound += cardxgutter * pgcol
+                                }
+                                var cardybound = CGFloat(cardysize * pgrow + pgymargin)
+                                if (pgrow >= CGFloat(1)) {
+                                    cardybound += cardygutter * pgrow
+                                }
+                                
+                                
+                                let pagesIndexRise = (cardsperx * cardspery * CGFloat(pgnum))
+                                let cardindex = pagesIndexRise + pgrow * cardsperx + pgcol + 1
+                                
+                                if (cardindex >= CGFloat(self.rows.count)) {
+                                    break
+                                }
+                                
+                                self.drawShape("linerect", context: context!, xfrom: cardxbound, yfrom: cardybound, xsize: cardxsize, ysize: cardysize)
+                                
+                                if (pgrow == CGFloat(0) && pgcol == CGFloat(0)) {
+                                    self.drawShape("fillrect", context: context!, xfrom: cardxbound, yfrom: cardybound, xsize: cardxsize, ysize: cardysize)
+                                }
+                                
+                                let textAttributes = ["font": "Lato", "size": "18", "weight": "Light", "color": "black"]
+                                self.drawShape("textframe", context: context!, xfrom: cardxbound, yfrom: cardybound, xsize: cardxsize, ysize: cardysize, text: self.rows[Int(cardindex)]["Name"] as! String, textattributes: textAttributes)
+                                
+                            } // pgrows
+                        } // pgcols
+                        
+                        CGPDFContextEndPage(context)
+                    } // pages
                     
-                    CGContextSetRGBFillColor (context, 1, 0, 0, 1);
-                    CGContextFillRect (context, CGRectMake (0, 0, 200, 100 ));
-                    CGContextSetRGBFillColor (context, 0, 0, 1, 0.5);
-                    CGContextFillRect (context, CGRectMake (0, 0, 100, 200));
-                    
-                    
-                    
-                    CGPDFContextEndPage(context)
                     CGPDFContextClose(context)
                     
-                    // remember to delete the temp folder
+                    // TODO: remember to delete the temp folder
                     
                 }
             }
         }
         
     }
+    
+    // helper function transforming mm to quartz 2d pt
+    
+    func drawShape(shape: String, context: CGContextRef, xfrom: CGFloat, yfrom: CGFloat, xsize: CGFloat, ysize: CGFloat) {
+        drawShape(shape, context: context, xfrom: xfrom, yfrom: yfrom, xsize: xsize, ysize: ysize, text: "", textattributes: ["": ""])
+    }
+    
+    func drawShape(shape: String, context: CGContextRef, xfrom: CGFloat, yfrom: CGFloat, xsize: CGFloat, ysize: CGFloat, text: String) {
+        drawShape(shape, context: context, xfrom: xfrom, yfrom: yfrom, xsize: xsize, ysize: ysize, text: text, textattributes: ["": ""])
+    }
+    
+    func drawShape(shape: String, context: CGContextRef, xfrom: CGFloat, yfrom: CGFloat, xsize: CGFloat, ysize: CGFloat, text: String, textattributes: [String: String]) {
+        let thinline = CGFloat(0.25)
+        // colors as params; params as array/dictionary?
+        
+        switch shape {
+            
+        case "linerect":
+            CGContextSetLineWidth(context, thinline)
+            CGContextSetStrokeColorWithColor(context, NSColor.redColor().CGColor)
+            CGContextAddRect(context, CGRectMake(xfrom, yfrom, xsize, ysize))
+            CGContextStrokePath(context)
+            
+        case "fillrect":
+            CGContextAddRect(context, CGRectMake(xfrom, yfrom, xsize, ysize))
+            CGContextSetRGBFillColor (context, 1, 0, 0, 1)
+            CGContextFillPath(context)
+            
+        case "textframe":
+            var font = NSFont(name: "Lato", size: 10.0)
+            if (textattributes["font"] != nil) {
+                font = NSFont(name: textattributes["font"]!, size: 10.0)
+            }
+            if (textattributes["weight"] != nil) {
+                let fontNameWithWeight = font!.fontName.componentsSeparatedByString("-").first! + "-" + textattributes["weight"]!
+                font = NSFont(name: fontNameWithWeight, size: 10.0)
+            }
+            if (textattributes["size"] != nil) {
+                let customsize = textattributes["size"]! as NSString
+                font = NSFont(name: font!.fontName, size: CGFloat(customsize.intValue))
+            }
+            
+            
+            let textStyle = NSMutableParagraphStyle.defaultParagraphStyle().mutableCopy() as! NSMutableParagraphStyle
+            let textColor = NSColor.blackColor()
+            
+            let textFontAttributes = [
+                NSFontAttributeName : font!,
+                NSForegroundColorAttributeName: textColor,
+                NSParagraphStyleAttributeName: textStyle
+            ]
+            
+            CGContextSetTextMatrix(context, CGAffineTransformIdentity)
+            
+            let attributedString = NSMutableAttributedString(string: text, attributes: textFontAttributes)
+            
+            let framesetter =  CTFramesetterCreateWithAttributedString(attributedString);
+            let path = CGPathCreateWithRect(CGRectMake(xfrom, yfrom, xsize, ysize), nil);
+            let frame = CTFramesetterCreateFrame(framesetter, CFRangeMake(0, 0), path, nil);
+            CTFrameDraw(frame, context);
+            
+            
+
+            
+            
+            
+        default:
+            break
+        }
+        
+    }
+    
+    
+    func mm2pt(mm: CGFloat) -> CGFloat {
+        let ratio = 2.8333
+        return CGFloat(mm * CGFloat(ratio))
+    }
+    
     
     // helper function translating col number to excel column index
     func toExcelCol(colNum: Int) -> String {
