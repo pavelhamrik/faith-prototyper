6a6a9c65dc6f1058cbd948c12dacd7c0b3d87594
Pavel Hamrik <pavel.hamrik@gmail.com>
2015-12-23 08:27:42 +0100

    Preferences 2.

--- Faith Prototyper/Faith Prototyper/Preferences.swift
+++ Faith Prototyper/Faith Prototyper/Preferences.swift
@@ -8,11 +8,48 @@
 
 import Cocoa
 
-class Preferences: NSViewController {
+class Preferences: NSViewController, NSTextFieldDelegate, NSComboBoxDelegate {
+    
+    @IBOutlet weak var types: NSComboBox!
+    
+    @IBOutlet weak var groups: NSTextField!
+    
+    @IBOutlet weak var statuses: NSComboBox!
+    
+    @IBOutlet weak var useImages: NSButton!
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do view setup here.
+        
+        self.types.setDelegate(self)
+        //self.groups.setDelegate(self)
+        self.statuses.setDelegate(self)
+        
+        let defaultTypes = Helpers.loadDefaults("prefsExportTypes")
+        if (!(defaultTypes ?? "").isEmpty) {
+            types.stringValue = defaultTypes
+        }
+
+        let defaultStatuses = Helpers.loadDefaults("prefsExportStatuses")
+        if (!(defaultStatuses ?? "").isEmpty) {
+            statuses.stringValue = defaultStatuses
+        }
+
+        // TODO: useImages
+        
     }
+    
+    override func controlTextDidChange(notification: NSNotification?) {
+        
+        if notification?.object as? NSComboBox == self.groups {
+            Helpers.saveDefaults("prefsExportGroups", value: self.groups.stringValue)
+        }
+        
+    }
+    
+
+    
+    
     
 }
